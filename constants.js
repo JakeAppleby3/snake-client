855b32f6659cf37c5898100ba08ef31ef9a6e59d
Jake Appleby <jakeappleby@live.com>
2021-07-01 05:35:24 +0000

    Constants module.

---
+++ constants.js
@@ -0,0 +1,15 @@
+const IP = '135.23.223.133';
+const PORT = '50542';
+const UP_KEY = 'Move: up';
+const DOWN_KEY = 'Move: down';
+const LEFT_KEY = 'Move: left';
+const RIGHT_KEY = 'Move: right';
+
+module.exports = {
+  IP,
+  PORT,
+  UP_KEY,
+  DOWN_KEY,
+  LEFT_KEY,
+  RIGHT_KEY,
+};
